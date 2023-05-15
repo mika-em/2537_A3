4db8818ac051e78961288ff29a223cfd6f4290ff
mika-em <mkmanaligod@gmail.com>
2023-05-15 14:35:56 -0700

    displayed pokemon images in cards

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const setup = async () => {
     pokemons.forEach((pokemon, index) => {
         $("#main").append(`
         <div class="card shadow-lg p-3 mb-5" style="width: 18rem;">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" class="card-img-top"
+        alt="...">
         <div class="card-body">
         <h5 class="card-title">${pokemon.name}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
