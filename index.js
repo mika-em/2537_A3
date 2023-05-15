6fa6d6a217bea1ac034948117d6b6cf6802a287c
mika-em <mkmanaligod@gmail.com>
2023-05-15 16:05:52 -0700

    created a modal to display each pokemon's details

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const setup = async () => {
     const result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810")
     console.log(result.data.results);
 
-    const pokemons = result.data.results;
+    // const pokemons = result.data.results;
 
-    pokemons.forEach((pokemon, index) => {
+    //slice me the first 3 pokemons
+    const pokemons = result.data.results.slice(0, 3);
+
+    pokemons.forEach(async (pokemon, index) => {
+
+        const pokemonResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         $("#main").append(`
         <div class="card shadow-lg p-3 mb-5" style="width: 18rem;">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" class="card-img-top"
@@ -15,6 +20,35 @@ const setup = async () => {
         <h5 class="card-title">${pokemon.name}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${pokemon.name}">
+            Launch demo modal
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal${pokemon.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">${pokemon.name}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    ${pokemonResult.data.id}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
         </div>
     </div>
     `)
