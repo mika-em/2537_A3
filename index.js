bd7826d20c5ef8f924b618835c12442f0f76ea1a
mika-em <mkmanaligod@gmail.com>
2023-05-15 14:35:31 -0700

    fetched all pokemon names from API

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const setup = async () => {
+
     console.log("setup");
-    const result = await axios.get("http://localhost:3000/api/")
+    const result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810")
     console.log(result.data.results);
 
+    const pokemons = result.data.results;
+
+    pokemons.forEach((pokemon, index) => {
+        $("#main").append(`
+        <div class="card shadow-lg p-3 mb-5" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${pokemon.name}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    `)
+    })
 }
 
-$(document).ready(setup)
+
+$(document).ready(setup);
