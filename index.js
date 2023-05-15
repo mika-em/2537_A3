74a5fba6211d6244f2624beb80aadd1d30dc7072
mika-em <mkmanaligod@gmail.com>
2023-05-15 14:34:03 -0700

    verfied that axios is working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const setup = () => {
-    console.log("setup")
+const setup = async () => {
+    console.log("setup");
+    const result = await axios.get("http://localhost:3000/api/")
+    console.log(result.data.results);
+
 }
 
 $(document).ready(setup)
