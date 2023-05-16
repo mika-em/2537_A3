b881bf4649bd6b8741c4a2b6e6b49931a2a8dbe1
mika-em <mkmanaligod@gmail.com>
2023-05-15 19:55:15 -0700

    displyed pokemon types in a checkbox group

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+const fetchPokemonTypes = async () => {
+    try {
+        const response = await axios.get("https://pokeapi.co/api/v2/type/");
+        return response.data.results;
+    } catch (error) {
+        console.error("Error fetching Pokémon types:", error);
+        return [];
+    }
+};
+
+
+
 const setup = async () => {
+    // Fetch Pokémon types
+    const pokemonTypes = await fetchPokemonTypes();
+
+    // Generate checkbox group for types
+    const typeCheckboxes = pokemonTypes.map((type) => {
+        return `
+    <div class="form-check">
+    <input class="form-check-input" type="checkbox" value="${type.name}" id="${type.name}">
+    <label class="form-check-label" for="${type.name}">
+        ${type.name}
+    </label>
+    </div>
+`;
+    });
+
+    // Append the checkbox group to a container element
+    $("#typeContainer").html(typeCheckboxes.join(""));
+
+
     const result = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810");
 
     const pokemons = result.data.results;
     const page_size = 10;
     const buttons = Math.ceil(pokemons.length / page_size);
-
     let currentPage = 1;
 
     const updatePaginationButtons = () => {
@@ -21,54 +51,72 @@ const setup = async () => {
         }
     };
 
-
     const updateDisplayedPokemons = async () => {
-        $("#main").empty();
+        // Get the selected Pokémon types
+        const selectedTypes = [];
+        $("input[type='checkbox']:checked").each(function () {
+            selectedTypes.push($(this).val());
+        });
 
+        // Filter Pokémon based on selected types
+        const filteredPokemons = pokemons.filter((pokemon) => {
+            return selectedTypes.every((type) => pokemon.types.includes(type));
+        });
+
+        // Update the displayed count
         const startingIndex = (currentPage - 1) * page_size;
         const endIndex = startingIndex + page_size;
-        const sliced_pokemons = pokemons.slice(startingIndex, endIndex);
+        const displayedPokemons = filteredPokemons.slice(startingIndex, endIndex);
 
-        for (let i = 0; i < sliced_pokemons.length; i++) {
-            const pokemon = sliced_pokemons[i];
+        $("#displayedPokemons").text(displayedPokemons.length);
+        $("#totalPokemons").text(filteredPokemons.length);
+        $("#displayedPokemons").text(`${filteredPokemons.length}`);
+
+
+        $("#main").empty();
+        for (let i = 0; i < displayedPokemons.length; i++) {
+            const pokemon = displayedPokemons[i];
             const index = startingIndex + i;
             const pokemonResult = await axios.get(
                 `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
             );
             $("#main").append(`
-                <div class="card shadow-lg p-3 mb-5" style="width: 18rem;">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${pokemon.name}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${pokemon.name}">
-                            Launch demo modal
-                        </button>
+                                                    <div class="card shadow-lg p-3 mb-5" style="width: 18rem;">
+                                                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png" class="card-img-top" alt="...">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">${pokemon.name}</h5>
+                                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                                            <!-- Button trigger modal -->
+                                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${pokemon.name}">
+                                                                Launch demo modal
+                                                            </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal${pokemon.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">${pokemon.name}</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        ${pokemonResult.data.id}
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `);
+                                                            <!-- Modal -->
+                                                            <div class="modal fade" id="exampleModal${pokemon.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                <div class="modal-dialog">
+                                                                    <div class="modal-content">
+                                                                        <div class="modal-header">
+                                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">${pokemon.name}</h1>
+                                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                        </div>
+                                                                        <div class="modal-body">
+                                                                            ${pokemonResult.data.id}
+                                                                        </div>
+                                                                        <div class="modal-footer">
+                                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                `);
             $("#displayedPokemons").text(`${startingIndex + i + 1}`);
         }
     };
+
+
+
 
     const updatePokemonCounts = () => {
         $("#totalPokemons").text(`${pokemons.length}`);
